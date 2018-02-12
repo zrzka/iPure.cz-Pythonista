981eaaa6f2ef9639b4f34c2125bb951f8d533cac
Robert Vojta <rvojta@me.com>
2018-02-12 07:46:33 +0100

    Folder renamed

---
+++ 01-dil/notifikace.py
@@ -0,0 +1,13 @@
+import notification
+
+#
+# Za 5s od spusteni skriptu se zobrazi notifikace.
+#
+# Pokud bude Pythonista v popredi, notifikace se zobrazi jako dialog
+# primo v Pythonistovi. Po tapnuti na tlacitko OK se pouze zavre
+# dialog s notifikaci.
+#
+# Pokud bude Pythonista v pozadi, zobrazi se systemova notifikace. Po
+# tapnuti na notifikaci se spusti Pythonista.
+#
+notification.schedule('Mrkni co je nového na iPure.cz', delay=5)
---
+++ 01-dil/s-odkazem.py
@@ -0,0 +1,14 @@
+import notification
+
+#
+# Za 5s od spusteni skriptu se zobrazi notifikace.
+#
+# Pokud bude Pythonista v popredi, notifikace se zobrazi jako dialog
+# primo v Pythonistovi. Po tapnuti na tlacitko OK se otevre Safari
+# s odkazem http://www.ipure.cz/
+#
+# Pokud bude Pythonista v pozadi, zobrazi se systemova notifikace. Po
+# tapnuti na notifikaci se spusti Pythonista a hned pote se otevre
+# Safari s odkazem http://www.ipure.cz/
+#
+notification.schedule('Mrkni co je nového na iPure.cz', delay=5, action_url='http://www.ipure.cz/')
